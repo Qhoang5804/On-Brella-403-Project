ecd50420a7efaff269a0abcd14f815a9e7d645a2
biniyam <biniyam@uw.edu>
2026-02-15 18:02:43 -0800

    fixed simulator CI workflow

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: "node",
   testMatch: ["**/*.jest.test.js"],
-  testPathIgnorePatterns: ["/node_modules/", "/hardwareSimulation/"],
+  testPathIgnorePatterns: ["/node_modules/"],
   collectCoverageFrom: ["frontend/src/**/*.js", "backend/src/**/*.js"],
 };

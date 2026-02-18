3bfb8e03d873a6138e3d374081c0c273ca9b41d4
biniyam <biniyam@uw.edu>
2026-02-17 19:49:55 -0800

    fixed front end test issues

--- frontend/tests/frontend.test.js
+++
@@ -1,6 +0,0 @@
-const assert = require("assert");
-const { frontendInitialized } = require("../src/index.js");
-
-assert.strictEqual(frontendInitialized(), "frontend initialized", "should return placeholder string");
-
-console.log("frontend.test.js passed");
